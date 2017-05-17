466b5fc78dd854a98bcd6de068c7a20422e3a779
Leo24 <leo2410@yandex.ru>
2017-05-17 13:53:32 +0300

    Add neilpatel theme

---
+++ wp-content/themes/neilpatel/js/script-tooltipster.js
@@ -0,0 +1,16 @@
+(function($){
+	
+	// menu-footer:
+	
+	var lang_list = $('.menu-footer .lang-list');
+	
+	$('.menu-footer .menu-item-lang').tooltipster({
+		
+		content: lang_list,
+		interactive: true,
+		theme: 'tooltipster-mf',
+		offsetY: 4
+	
+	});
+
+})(jQuery);
